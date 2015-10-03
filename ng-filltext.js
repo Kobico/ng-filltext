4a340611bd94337db94f5e814fd2d05b111d82c6
Kobico <kobi987cohen@gmail.com>
2015-10-03 18:14:59 +0300

    Making filltext request as simple as possible
    Change the directive type to "Element"
    Enable to choose "Table" or "Card" view
    Passing "FillText" fields as simple string instead of object
    Option to pass "Titles" for the data

--- ng-filltext.js
+++ ng-filltext.js
@@ -1,4 +1,3 @@
-;
 angular.module('ng-filltext', []).
 service('filltext', ['$http', function($http) {
     return {
@@ -18,22 +17,45 @@ service('filltext', ['$http', function($http) {
         }
     }
 }]).
-directive('filltextTable', function(filltext) {
+directive('filltext', function(filltext) {
     return {
-        restrict: "C",
-        scope: {
-            item: "=",
-            key: "="
-        },
+        restrict: 'E',
+        replace: true,
+        scope: {},
         link: function($scope, element, attributes) {
-            filltext.create($scope.$eval(attributes.dataset), function(data) {
-                $scope.FTData = data;
-                $scope.order = $scope.$eval(attributes.order);
-                $scope.fto = function(val) {
-                    return $scope.order.indexOf(val)
-                }
-            });
+           var fields = attributes.fields.split(','); // Mandatory: firstName, lastName, streetAddress etc.
+           var titles = attributes.titles ? attributes.titles.split(',') : null; // Opt.
+           var rows = attributes.rows ? attributes.rows : 10; //Default 10
+           var delay = (attributes.delay && attributes.delay <= 30) ? attributes.delay : null; //Up to 30 secs
+           var opt = fields.toObject(fields);
+           filltext.create(angular.extend(opt, {delay:delay}, {rows:rows}), function(data) {
+            $scope.fields = fields;
+            $scope.hds = titles || fields;
+            $scope.data = data;
+           });
         },
-        template: '<tr ng-repeat="item in FTData">' + '<td ng-repeat="key in order">{{item[key]}}</td>' + '</tr>'
+        template: function(element, attributes) {
+            var view = attributes.view ? attributes.view : 'Table'; //Table or Card
+            if(view=='Table') {
+                console.log('rendering as table');
+                return '<table><thead><th ng-repeat="h in hds">{{h}}</th></thead><tr ng-repeat="row in data"><td ng-repeat="key in fields">{{row[key]}}</td></tr></table>';
+            }
+            else {
+                return '<div ng-repeat="card in data"><h1>{{card[fields[0]]}} {{card[fields[1]]}}</h1><ul><li ng-repeat="key in fields.slice(2)">{{key}} : {{card[key]}}</li></ul></div>';
+            }
+        }
+                    
     }
-}); 
+});
+Array.prototype.toObject = function(keys){
+    var obj = {};
+    var tmp = this;
+    if(keys.length == this.length){
+        var c = this.length-1;
+        while( c>=0 ){
+            obj[ keys[ c ] ] = "{"+tmp[c]+"}";
+            c--;
+        }
+    }
+    return obj;
+};
